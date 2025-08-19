8eb8e1c8b862a7c58d6bd1440105a4a13b0a50a9
AndersonVelozo <andersonlinharesvelozo@hotmail.com>
2025-08-19 13:45:34 -0300

    ArrayList

--- src/EstudoArrayList.java
+++ src/EstudoArrayList.java
@@ -6,6 +6,37 @@ public class EstudoArrayList {
     public static void main(String[] args) {
 
         List<String> listaDeCompras = new ArrayList<>();
+        listaDeCompras.add("Mamão");
+        listaDeCompras.add("Sabão em Pó");
+        listaDeCompras.add("Macarrão");
 
+
+
+        listaDeCompras.add(1, "Banana");
+
+        for(String item : listaDeCompras){
+            System.out.println(item);
+        }
+
+        String item = listaDeCompras.get(3);
+        System.out.println("Na posição 3 eu tenho o: " + item);
+
+        listaDeCompras.remove("Sabão em Pó");
+
+        for (String item2 : listaDeCompras) {
+            System.out.println(item2);
+        }
+
+        boolean EstaVazia = listaDeCompras.isEmpty();
+        int tamanho = listaDeCompras.size();
+        boolean contem = listaDeCompras.contains("Arroz");
+
+        System.out.println("A lista está vazia? " + EstaVazia + ", Qual o tamanho da lista? " + tamanho + ", A lista contem o item? " +contem);
+
+//        listaDeCompras.clear();
+//        System.out.println("A lista está vazia? " + listaDeCompras.isEmpty());
+
+        listaDeCompras.forEach(produto -> System.out.println(produto));
     }
+
 }
