5eb7a9969bfb25982c3cea7552593d19c6d52981
AndersonVelozo <andersonlinharesvelozo@hotmail.com>
2025-09-06 14:01:17 -0300

    Pilares do POO: Herança

---
+++ src/Heranca/Cachorro.java
@@ -0,0 +1,7 @@
+package Heranca;
+
+public class Cachorro extends Animal{
+    public void latir(){
+        System.out.println("Nosso cachorro está latindo");
+    }
+}
---
+++ src/Heranca/Principal.java
@@ -0,0 +1,19 @@
+package Heranca;
+
+public class Principal {
+    public static void main(String[] args) {
+
+        Cachorro caramelo = new Cachorro();
+        caramelo.latir();
+        caramelo.comer("Ração");
+        caramelo.beberAgua();
+        caramelo.dormir();
+
+        Galinha gertrudez = new Galinha();
+        gertrudez.ciscar();
+        caramelo.comer("Grão de milho");
+        caramelo.beberAgua();
+        caramelo.dormir();
+
+    }
+}
