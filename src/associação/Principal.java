bc024d3511f298e751358308070b79098b3c87ca
AndersonVelozo <andersonlinharesvelozo@hotmail.com>
2025-09-15 22:08:35 -0300

    Pilares do POO: Agregação

--- src/associação/Principal.java
+++ src/associação/Principal.java
@@ -1,10 +1,30 @@
 package associação;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 public class Principal {
     public static void main (String[] args) {
 
         Carro fusca = new Carro(40);
-
         fusca.mostrarCilindradas();
+
+
+        Funcionarios joao = new Funcionarios("João");
+        Funcionarios anderson = new Funcionarios("Anderson");
+        Funcionarios fabio = new Funcionarios("Fabio");
+
+        List<Funcionarios> listaDeFuncionarios = new ArrayList<>();
+        listaDeFuncionarios.add(joao);
+        listaDeFuncionarios.add(anderson);
+        listaDeFuncionarios.add(fabio);
+
+        Departamento ti = new Departamento("TI", listaDeFuncionarios);
+        ti.mostrarFuncionarios();
+
+        Departamento marketing = new Departamento("Marketing", Collections.emptyList());
+        marketing.mostrarFuncionarios();
     }
 }
